6abfe8beecee2ac4e4420d4653f1df19f40bbc83
Nurbek-web <taizhanovntm@gmail.com>
2024-04-20 01:04:28 +0500

    components

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def classify_image(img):
     return dict(zip(categories, map(float, probs)))
 
 examples = ["maxi.jpg", "kairat.jpeg"]
-intf = gr.Interface(fn=classify_image, inputs=gr.inputs.Image(shape=(192, 192)), outputs=gr.outputs.Label(), examples=examples)
+intf = gr.Interface(fn=classify_image, inputs=gr.components.Image(shape=(192, 192)), outputs=gr.components.Label(), examples=examples)
 intf.launch(inline=False,share=True)
