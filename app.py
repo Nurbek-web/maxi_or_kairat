46352e630cdbe3c3dff0c43380a50cc305015dd5
Nurbek-web <taizhanovntm@gmail.com>
2024-04-19 20:37:55 +0500

    added model, created py script

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import gradio as gr
+from fastai.vision.all import *
+from fastbook import *
+from fastai.vision.widgets import *
+import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+learn_inf = load_learner('export.pkl')
+
+categories = ("maxi", "kairat")
+
+def classify_image(img):
+    pred,idx,probs = learn_inf.predict(img)
+    return dict(zip(categories, map(float, probs)))
+
+examples = ["maxi.jpg", "kairat.jpeg"]
+intf = gr.Interface(fn=classify_image, inputs=gr.inputs.Image(shape=(192, 192)), outputs=gr.outputs.Label(), examples=examples)
+intf.launch(inline=False,share=True)
