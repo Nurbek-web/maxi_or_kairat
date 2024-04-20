29bee1df0e95e050c10dadcdf72ab9426847d0bb
Nurbek-web <taizhanovntm@gmail.com>
2024-04-20 22:26:38 +0500

    setted up all UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,80 @@
+import { useState } from "react";
+
 export default function App() {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImage(reader.result);
+      // Automatically submit the form when the image is selected
+      handleSubmit();
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = () => {
+    // If there's no image, return
+    if (!image) return;
+
+    // Sending the image to the server
+    fetch("your-server-url", {
+      method: "POST",
+      body: JSON.stringify({ image }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        // Handle response from the server
+        console.log(response);
+      })
+      .catch((error) => {
+        // Handle error
+        console.error("Error:", error);
+      });
+  };
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Maxi чай или же Кайрат Нуртас?
-    </h1>
+    <div
+      style={{
+        backgroundColor: "#070F2B",
+        minHeight: "100vh",
+        color: "#9290C3",
+      }}
+    >
+      <h2 className="text-3xl text-center font-bold">
+        Maxi чай или же Кайрат Нуртас?
+      </h2>
+      <div
+        className="m-3"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        {image && (
+          <img
+            src={image}
+            alt="Uploaded Image"
+            style={{
+              width: "15rem",
+            }}
+          />
+        )}
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()} // Prevent default submission
+        className="space-y-8 font-[sans-serif] max-w-md mx-auto"
+      >
+        <input
+          type="file"
+          className="w-full text-black text-base bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-2.5 file:px-4 file:mr-4 file:bg-gray-800 file:hover:bg-gray-700 file:text-white rounded"
+          onChange={handleImageChange}
+        />
+      </form>
+    </div>
   );
 }
